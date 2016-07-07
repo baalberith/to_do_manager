c7bbb31d8789b8298c8b98b8b30f6827260621a0
Ilona <ilonakoda89@gmail.com>
2016-07-07 11:23:02 +0200

    some fixes

--- web/controllers/task_controller.ex
+++ web/controllers/task_controller.ex
@@ -18,7 +18,6 @@ defmodule ToDoManager.TaskController do
     end
   end
 
-
   def update(conn, %{"id" => id, "task" => task_params}) do
     task = Repo.get!(Task, id)
     changeset = Task.changeset(task, task_params)
@@ -34,21 +33,20 @@ defmodule ToDoManager.TaskController do
   def delete(conn, %{"list_id" => list_id, "id" => id}) do
     task = Repo.get!(Task, id)
     Repo.delete!(task)
-    conn
-    |> put_flash(:info, "Task deleted successfully.")
-    |> redirect(to: list_path(conn, :show, list_id))
-  end
 
+    json conn, %{info: "Task deleted successfully."}
+  end
 
   def complete_tasks(conn, %{"list_id" => list_id, "tasks_to_complete" => tasks_to_complete}) do
     for task_id <- tasks_to_complete do
       task = Repo.get!(Task, String.to_integer(task_id))
       Repo.update!(%{task | completed: true})
     end
-    json conn, %{location: list_path(conn, :show, list_id), info: "Tasks completed successfully."}
+
+    json conn, %{info: "Tasks completed successfully."}
   end
   def complete_tasks(conn, %{"list_id" => list_id}) do
-    json conn, %{location: list_path(conn, :show, list_id), info: "Nothing to complete."}
+    json conn, %{info: "Nothing to complete."}
   end
 
   def delete_tasks(conn, %{"list_id" => list_id, "tasks_to_delete" => tasks_to_delete}) do
@@ -56,10 +54,11 @@ defmodule ToDoManager.TaskController do
       task = Repo.get!(Task, String.to_integer(task_id))
       Repo.delete!(task)
     end
-    json conn, %{location: list_path(conn, :show, list_id), info: "Tasks deleted successfully."}
+
+    json conn, %{info: "Tasks deleted successfully."}
   end
   def delete_tasks(conn, %{"list_id" => list_id}) do
-    json conn, %{location: list_path(conn, :show, list_id), info: "Nothing to delete."}
+    json conn, %{info: "Nothing to delete."}
   end
 
 
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -16,17 +16,28 @@ import $ from "jquery"
 import React from "react"
 import ReactDOM from "react-dom"
 
+const csrf = $("meta[name=csrf]").attr('content');
+
 const Task = React.createClass({
   render() {
     return (
       <tr>
-        <td><input data-number={this.props.value} onClick={this.props.onCompChbxClick} type="checkbox" name="to_complete[]" value={this.props.task.id} /></td>
-        <td><input data-number={this.props.value} onClick={this.props.onDelChbxClick} type="checkbox" name="to_delete[]" value={this.props.task.id} /></td>
-        <td>{this.props.task.name}</td>
-        <td>{this.props.task.completed ? "Completed" : this.props.task.date}</td>
+        <td>
+          <input data-number={this.props.value} onClick={this.props.onCompChbxClick} type="checkbox" name="to_complete[]" value={this.props.task.id} />
+        </td>
+        <td>
+          <input data-number={this.props.value} onClick={this.props.onDelChbxClick} type="checkbox" name="to_delete[]" value={this.props.task.id} />
+        </td>
+        <td>
+          {this.props.task.name}
+        </td>
+        <td>
+          {this.props.task.completed ? "Completed" : this.props.task.date}
+        </td>
         <td className="text-right">
-        <button data-number={this.props.value} onClick={this.props.onEditTask} name="task" value={this.props.task.id}> Edit </button>
-        <button data-number={this.props.value} onClick={this.props.deleteTask} name="task" value={this.props.task.id}> Delete </button></td>
+          <button data-number={this.props.value} onClick={this.props.onEditTask} name="task" value={this.props.task.id}> Edit </button>{" "}
+          <button data-number={this.props.value} onClick={this.props.deleteTask} name="task" value={this.props.task.id}> Delete </button>
+        </td>
       </tr>
     )
   }
@@ -35,14 +46,32 @@ const Task = React.createClass({
 const NewTask = React.createClass({
   render() {
     return (
-        <form onSubmit={this.props.toEdit ? this.props.editTask : this.props.addTask}>
-        <table className="table"><thead><tr><th>Name</th><th>Due date</th><th></th></tr></thead><tbody><tr>
-        <td><input onChange={this.props.onNameChange} type="text" name="name" value={this.props.task.name} />
-        <span className="help-block">{this.props.errors.name}</span></td>
-        <td><input onChange={this.props.onDateChange} type="date" name="date" value={this.props.task.date} /><span className="help-block">{this.props.errors.date}</span></td>
-        <td className="text-right"><button>{this.props.toEdit ? "Edit task" : "Add task"}</button></td>
-        </tr></tbody></table>
-        </form>
+      <form onSubmit={this.props.toEdit ? this.props.editTask : this.props.addTask}>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Due date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input onChange={this.props.onNameChange} type="text" name="name" value={this.props.task.name} />
+                <span className="help-block">{this.props.errors.name}</span>
+              </td>
+              <td>
+                <input onChange={this.props.onDateChange} type="date" name="date" value={this.props.task.date} />
+                <span className="help-block">{this.props.errors.date}</span>
+              </td>
+              <td className="text-right">
+                <button>{this.props.toEdit ? "Edit task" : "Add task"}</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     );
   }
 });
@@ -51,225 +80,322 @@ const ListApp = React.createClass({
   getInitialState() {
     return {
       tasks: this.props.list.tasks,
-      task: { name: '', date: '' },
-      errors: { name: '', date: '' },
+      task: {
+        name: '',
+        date: '' },
+      errors: {
+        name: '',
+        date: '' },
       toEdit: undefined
     }
   },
+
   onNameChange(event) {
-    this.setState({task: {name: event.target.value, date: this.state.task.date}});
+    this.setState({
+      task: {
+        name: event.target.value,
+        date: this.state.task.date
+      }
+    });
   },
+
   onDateChange(event) {
-    this.setState({task: {name: this.state.task.name, date: event.target.value}});
+    this.setState({
+      task: {
+        name: this.state.task.name,
+        date: event.target.value
+      }
+    });
   },
+
   onCompChbxClick(event) {
     var taskIndex = parseInt(event.target.dataset.number, 10);
+
     if (this.state.tasks[taskIndex].toComplete == undefined) {
       this.state.tasks[taskIndex].toComplete = true;
-    } else {
+    }
+    else {
       this.state.tasks[taskIndex].toComplete = !(this.state.tasks[taskIndex].toComplete);
     }
+
     this.setState({tasks: this.state.tasks});
   },
+
   onDelChbxClick(event) {
     var taskIndex = parseInt(event.target.dataset.number, 10);
+
     if (this.state.tasks[taskIndex].toDelete == undefined) {
       this.state.tasks[taskIndex].toDelete = true;
-    } else {
+    }
+    else {
       this.state.tasks[taskIndex].toDelete = !(this.state.tasks[taskIndex].toDelete);
     }
+
     this.setState({tasks: this.state.tasks});
-    console.log(this.state.tasks);
   },
+
   onEditTask(event) {
     var taskIndex = parseInt(event.target.dataset.number, 10);
-    this.setState(
-      {task: {
+
+    this.setState({
+      task: {
         name: this.state.tasks[taskIndex].name,
         date: this.state.tasks[taskIndex].date,
       },
        toEdit: {index: taskIndex, id: event.target.value}
-      });
+    });
   },
+
   editTask(event) {
-    console.log(this.state.toEdit);
     var path = $(location).attr('pathname') + '/tasks/' + this.state.toEdit.id;
-    var csrf = $("meta[name=csrf]").attr('content');
-    var newTask = {
+
+    var updatedTask = {
       name: this.state.task.name,
       date: this.state.task.date,
       list_id: this.props.list.id,
       completed: false
     };
+
     $.ajax({
       url: path,
       type: 'POST',
-      data: { _csrf_token: csrf, _method: "patch", task: newTask },
+      data: { _csrf_token: csrf, _method: "patch", task: updatedTask },
+
       success: function(data) {
-        this.state.errors = { name: '', date: '' };
+        this.state.errors = {
+          name: '', date: ''
+        };
+
         if (data.valid) {
-          this.state.tasks[this.state.toEdit.index] = {id: data.task.id, name: data.task.name, date: data.task.date }
+          this.state.tasks[this.state.toEdit.index] = data.task;
+
           this.setState({
             tasks: this.state.tasks,
-            task: { name: '', date: '' },
+            task: {
+              name: '',
+              date: '' },
             toEdit: undefined
           });
-        } else {
+        }
+        else {
           for (let error of data.errors) {
             var value = this.state.errors[error.field];
             this.state.errors[error.field] = value + "\n" + error.detail;
           }
+
           this.setState({
             errors: this.state.errors
           })
         }
       }.bind(this),
+
       error: function(xhr, status, err) {
-        console.error("Can't create new task.");
+        console.error("Can't edit this task.");
       }.bind(this)
     });
+
     event.preventDefault();
   },
+
   deleteTask(event) {
     var taskIndex = parseInt(event.target.dataset.number, 10);
+
     if (confirm("Are you sure?")) {
       var path = $(location).attr('pathname') + '/tasks/' + event.target.value;
-      var csrf = $("meta[name=csrf]").attr('content');
+
       $.ajax({
         url: path,
         type: 'POST',
         data: { _csrf_token: csrf, _method: 'delete' },
+
         success: function(data) {
           this.setState(function(state) {
             state.tasks.splice(taskIndex, 1);
-            return { tasks: state.tasks };
+
+            return {
+              tasks: state.tasks,
+              toEdit: undefined
+            };
           });
         }.bind(this),
+
         error: function(xhr, status, err) {
-          console.error("Can't delete");
+          console.error("Can't delete this task.");
         }.bind(this)
       });
     }
   },
+
   addTask(event) {
-    console.log(event.target);
     var path = $(location).attr('pathname') + '/tasks';
-    var csrf = $("meta[name=csrf]").attr('content');
+
     var newTask = {
       name: this.state.task.name,
       date: this.state.task.date,
       list_id: this.props.list.id,
       completed: false
     };
+
     $.ajax({
       url: path,
       type: 'POST',
       data: { _csrf_token: csrf, task: newTask },
+
       success: function(data) {
-        this.state.errors = { name: '', date: '' };
+        this.state.errors = {
+          name: '',
+          date: ''
+        };
+
         if (data.valid) {
+          console.log(data.task);
           this.setState({
-            tasks: this.state.tasks.concat([{id: data.task.id, name: data.task.name, date: data.task.date }]),
-            task: { name: '', date: '' }
+            tasks: this.state.tasks.concat([data.task]),
+            task: {
+              name: '',
+              date: ''
+            }
           });
-        } else {
+        }
+        else {
           for (let error of data.errors) {
             var value = this.state.errors[error.field];
             this.state.errors[error.field] = value + "\n" + error.detail;
           }
+
           this.setState({
             errors: this.state.errors
           })
         }
       }.bind(this),
+
       error: function(xhr, status, err) {
         console.error("Can't create new task.");
       }.bind(this)
     });
+
     event.preventDefault();
   },
+
   completeSelectedTasks() {
     var tasksToComplete = [];
     var tasksNumbers = [];
+
     for (var taskIndex = 0; taskIndex < this.state.tasks.length; taskIndex++) {
       if (this.state.tasks[taskIndex].toComplete && !(this.state.tasks[taskIndex].completed)) {
         tasksToComplete.push(this.state.tasks[taskIndex].id);
         tasksNumbers.push(taskIndex);
       }
     }
+
     var path = $(location).attr('pathname') + '/tasks/complete_tasks';
-    var csrf = $("meta[name=csrf]").attr('content');
+
     $.ajax({
       url: path,
       type: 'POST',
       data: { _csrf_token: csrf, _method: 'patch', tasks_to_complete: tasksToComplete },
+
       success: function(data) {
         for (let taskIndex of tasksNumbers) {
           this.state.tasks[taskIndex].completed = true;
-          this.state.tasks[taskIndex].toComplete = true;
-          this.setState({ tasks: this.state.tasks });
+          this.state.tasks[taskIndex].toComplete = false;
+
+          this.setState({
+            tasks: this.state.tasks
+          });
         }
       }.bind(this),
+
       error: function(xhr, status, err) {
         console.error("Can't complete selected tasks.");
       }.bind(this)
     });
+
     event.preventDefault();
   },
+
   deleteSelectedTasks() {
     if (confirm("Are you sure?")) {
       var tasksToDelete = [];
       var tasksNumbers = [];
+
       for (var taskIndex = 0; taskIndex < this.state.tasks.length; taskIndex++) {
         if (this.state.tasks[taskIndex].toDelete) {
           tasksToDelete.push(this.state.tasks[taskIndex].id);
           tasksNumbers.push(taskIndex);
         }
       }
+
       var path = $(location).attr('pathname') + '/tasks/delete_tasks';
-      var csrf = $("meta[name=csrf]").attr('content');
+
       $.ajax({
         url: path,
         type: 'POST',
         data: { _csrf_token: csrf, _method: 'delete', tasks_to_delete: tasksToDelete },
+
         success: function(data) {
           for (var i = tasksNumbers.length - 1; i >= 0; i--) {
             this.state.tasks.splice(tasksNumbers[i], 1);
-            this.setState({ tasks: this.state.tasks });
+
+            this.setState({
+              tasks: this.state.tasks,
+              toEdit: undefined });
           }
         }.bind(this),
+
         error: function(xhr, status, err) {
           console.error("Can't delete selected tasks.");
         }.bind(this)
       });
     }
+
     event.preventDefault();
   },
+
   render() {
     return (
-      <div>
-      <table key={this.props.list.id} className="table">
-        <caption>{this.props.list.name}</caption>
-        <thead><tr><th>Complete</th><th>Delete</th><th>Task</th><th>Due date</th><th></th></tr></thead>
-        <tfoot><tr>
-        <td>{this.state.tasks.length > 0 ? <button onClick={this.completeSelectedTasks}>✓</button> : ""}</td>
-        <td>{this.state.tasks.length > 0 ? <button onClick={this.deleteSelectedTasks}>✓</button> : ""}</td>
-        <td></td><td></td><td></td></tr></tfoot>
-        <tbody>
-        {this.state.tasks.map(function(task, taskIndex) {
-          return <Task key={task.id} task={task} value={taskIndex} deleteTask={this.deleteTask} onEditTask={this.onEditTask} onCompChbxClick={this.onCompChbxClick} onDelChbxClick={this.onDelChbxClick} />
-          }.bind(this))}
-        </tbody>
-      </table>
-        <NewTask addTask={this.addTask} editTask={this.editTask} onNameChange={this.onNameChange} onDateChange={this.onDateChange} task={this.state.task} errors={this.state.errors} toEdit={this.state.toEdit} /></div>
+        <div>
+          <h2>{this.props.list.name}</h2>
+          <table key={this.props.list.id} className="table">
+            <thead>
+              <tr>
+                <th>Complete</th>
+                <th>Delete</th>
+                <th>Task</th>
+                <th>Due date</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <td>
+                  {this.state.tasks.length > 0 ? <button onClick={this.completeSelectedTasks}>✓</button> : ""}
+                </td>
+                <td>
+                  {this.state.tasks.length > 0 ? <button onClick={this.deleteSelectedTasks}>✓</button> : ""}
+                </td>
+                <td></td><td></td><td></td>
+              </tr>
+            </tfoot>
+            <tbody>
+              {this.state.tasks.map(function(task, taskIndex) {
+                return <Task key={task.id} task={task} value={taskIndex} deleteTask={this.deleteTask} onEditTask={this.onEditTask} onCompChbxClick={this.onCompChbxClick} onDelChbxClick={this.onDelChbxClick} />
+              }.bind(this))}
+            </tbody>
+          </table>
+        <h4>New task:</h4>
+          <NewTask addTask={this.addTask} editTask={this.editTask} onNameChange={this.onNameChange} onDateChange={this.onDateChange} task={this.state.task} errors={this.state.errors} toEdit={this.state.toEdit} />
+        </div>
     )
   }
 });
 
 var listProps = $("#list-props").attr("data-props");
+
 if (listProps) {
   ReactDOM.render(
-    <ListApp list={JSON.parse(listProps)} />, $("#list-component")[0])
+    <ListApp list={JSON.parse(listProps)} />,
+    $("#list-component")[0])
 }
 
 // Import local files
